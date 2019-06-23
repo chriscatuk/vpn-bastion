3c425246be4335644f6a2375f8edd4d4fdbc1673
Christian Gonzalvez <christian.gonzalvez@cloudreach.com>
2019-06-23 14:48:32 +0100

    0.12upgrade module gp-instance

--- modules/gp-instance/main.tf
+++ modules/gp-instance/main.tf
@@ -1,23 +1,25 @@
 resource "aws_instance" "vpn_server" {
-  ami           = "${data.aws_ami.ami_amzn2.id}"
-	instance_type = "${var.instance_type}"
-  key_name = "${aws_key_pair.key.key_name}"
-  subnet_id = "${var.subnet_id}"
+  ami                         = data.aws_ami.ami_amzn2.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = var.subnet_id
   associate_public_ip_address = true
-  ipv6_address_count = 1
-  vpc_security_group_ids = ["${var.sg_id}"]
-	tags = "${var.tags}"
-  lifecycle { create_before_destroy = true }
+  ipv6_address_count          = 1
+  vpc_security_group_ids      = [var.sg_id]
+  tags                        = var.tags
+  lifecycle {
+    create_before_destroy = true
+  }
   credit_specification {
     cpu_credits = "standard"
   }
 
-  user_data = "${data.template_file.user_data.rendered}"
+  user_data = data.template_file.user_data.rendered
 }
 
 data "template_file" "user_data" {
-  template = "${file("${local.template_path}")}"
-  vars = "${local.template_vars}"
+  template = file(local.template_path)
+  vars     = local.template_vars
 }
 
 ########################
@@ -28,8 +30,8 @@ data "template_file" "user_data" {
 # 31/07/2018: amzn2-ami-hvm-2.0.20180622.1-x86_64-gp2
 
 data "aws_ami" "ami_amzn2" {
-  most_recent      = true
-  owners           = ["amazon"]
+  most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
@@ -53,13 +55,12 @@ data "aws_ami" "ami_amzn2" {
 #   instance_id = "${aws_instance.vpn_server.id}"
 # }
 
-
 ########################
 #      ELASTIC IP      #
 ########################
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.vpn_server.id}"
-	tags = "${var.tags}"
+  instance = aws_instance.vpn_server.id
+  tags     = var.tags
 }
 
 # Define the Key Pair you will add in AWS
@@ -69,10 +70,9 @@ resource "aws_eip" "ip" {
 #       Key Pair       #
 ########################
 resource "aws_key_pair" "key" {
-   key_name   = "${var.keyname}"
-   public_key = "${var.keypublic}"
+  key_name   = var.keyname
+  public_key = var.keypublic
 }
-
 
 ########################
 #       DNS NAME       #
@@ -81,25 +81,26 @@ resource "aws_key_pair" "key" {
 # You can remove this file if you don't want to use DNS Names
 
 resource "aws_route53_record" "servername_ipv4" {
-  zone_id = "${var.route53_zoneID}"
+  zone_id = var.route53_zoneID
   name    = "${var.hostname}."
   type    = "A"
   ttl     = "300"
-  records = ["${aws_eip.ip.public_ip}"]
+  records = [aws_eip.ip.public_ip]
 }
 
 resource "aws_route53_record" "servername_ipv4_internal" {
-  zone_id = "${var.route53_zoneID}"
+  zone_id = var.route53_zoneID
   name    = "internal-${var.hostname}."
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.vpn_server.private_ip}"]
+  records = [aws_instance.vpn_server.private_ip]
 }
 
 resource "aws_route53_record" "servername_ipv6" {
-  zone_id = "${var.route53_zoneID}"
+  zone_id = var.route53_zoneID
   name    = "${var.hostname}."
   type    = "AAAA"
   ttl     = "300"
-  records = "${aws_instance.vpn_server.ipv6_addresses}"
+  records = aws_instance.vpn_server.ipv6_addresses
 }
+
