c182236b3d25f543d9d93722fc4f2a79722b7e58
Christian Gonzalvez <32960017+chriscatuk@users.noreply.github.com>
2019-10-28 13:59:27 +0000

    enforce secure algorithms

--- docker-ipsec-vpn-server/run.sh
+++ docker-ipsec-vpn-server/run.sh
@@ -131,8 +131,8 @@ conn shared
   dpddelay=30
   dpdtimeout=120
   dpdaction=clear
-  ike=3des-sha1,3des-sha2,aes-sha1,aes-sha1;modp1024,aes-sha2,aes-sha2;modp1024,aes256-sha2_512
-  phase2alg=3des-sha1,3des-sha2,aes-sha1,aes-sha2,aes256-sha2_512
+  ike=aes-sha256
+  phase2alg=aes-sha256
   sha2-truncbug=no
 
 conn l2tp-psk
